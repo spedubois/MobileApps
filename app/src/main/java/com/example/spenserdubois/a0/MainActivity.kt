2caef3030c2d01ce1d8e1bbf1c428e2110e45f2f
u0482113 <u0482113@1120-mac-09.lib.utah.edu>
2017-08-30 17:04:29 -0600

    comments

--- app/src/main/java/com/example/spenserdubois/a0/MainActivity.kt
+++ app/src/main/java/com/example/spenserdubois/a0/MainActivity.kt
@@ -4,6 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
+/**
+ * Class that hold the string and splits it into an array in order to check each word.
+ *
+ * Constructor: String or phrase we are looking to find the longest word in.
+ *
+ * MEMBER VARIABLES
+ * split: This is the array that hold all the individual words from the given phrase.
+ * The words are split by a white space.
+ *
+ * Author: Spenser DuBois
+ * Updated: 8/30/2017
+ */
 class Phrase(phrase: String){
 
     val split: List<String> = phrase.split(" ")
@@ -13,6 +25,13 @@ class Phrase(phrase: String){
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Checks to see a string contains any uppercase characters.
+     *
+     * Input: String we are checking
+     *
+     * Output: Integer representation of true (1) or false(0)
+     */
     fun checkUpper(word: String): Int{
 
         val deWord: String = word.toLowerCase()
@@ -20,10 +39,19 @@ class MainActivity : AppCompatActivity() {
         if(deWord.equals(word)) return 0 else return 1
     }
 
+    /**
+     * Checks to see if a String is comprised of ONLY letters.
+     *
+     * Input: String we are checking
+     *
+     * Output: Integer representation of true (1) or false(0)
+     */
     fun checkLetters(word: String): Int{
 
-        val wordArray = word.toCharArray()
+        //Regex used to find and numbers in the String
         val numReg = Regex(pattern = "\\d")
+
+        //Regex used to find and non-letter characters
         val puncReg = Regex(pattern = "\\W")
 
         if(numReg.containsMatchIn(input = word))
@@ -35,6 +63,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Checks to see if a String has an even length
+     *
+     * Input: String we are checking
+     *
+     * Output: Integer representation of true (1) or false(0)
+     */
     fun isEven(word: String): Int{
         if(word.length % 2 == 0) return 1 else return 0
     }
@@ -45,7 +80,7 @@ class MainActivity : AppCompatActivity() {
 
         val input: String = "anotHers Using Kotlin for Android Development Kotlin is a  " +
                 "anotHers great fit for developingAndroid applications, bringing all of the advantages of   " +
-                "a modern language to the Android platform without introducing any new restrictions: " +
+                "a modern language to the ThisIsTheLongestbutThereisOnlyONE Android platform without introducing any new restrictions: " +
                 "Compatibility: Kotlin is   fully compatible with JDK 6, ensuring that Kotlin applications can run on   " +
                 "older Android devices with no issues. The Kotlin tooling is fully supported in   " +
                 "Android Studio and compatible with the Android build system. " +
@@ -56,7 +91,7 @@ class MainActivity : AppCompatActivity() {
                 "use all existing Android libraries in   " +
                 "a Kotlin application. This includes annotation processing, so databinding and Dagger work too. " +
                 "Footprint: Kotlin has a very compactruntime library, which can be   " +
-                "further reduced through the use of ProGuard. In a  real application, the Kotlin runtime adds only a  " +
+                "further reduced through the use of ThisIsTheLongest ProGuard. In a  real application, the Kotlin runtime adds only a  " +
                 "few hundred methods and less than 100K to   " +
                 "the size of   the .apk file. Compilation Time: Kotlin supports efficient incremental compilation, " +
                 "so while there's some additional overhead for clean builds, incremental builds are usually as fast or   " +
@@ -81,24 +116,41 @@ class MainActivity : AppCompatActivity() {
                 "  creating a real Android application in   Kotlin. Check anotHers out Google's sample projects written in Kotlin."
 
 
+        /**
+         * Object used to hold a HashSet that contains String that are valid words,
+         * based on the assignment specifications, and the longest valid word found so far.
+         *
+         * VARIABLES
+         * foundWords: the HashSet of Strings found to be valid words.
+         * longest: The longest valid word found so far.
+         */
         val words = object{
             var foundWords: HashSet<String> = HashSet<String>()
             var longest: String = "";
         }
 
         val p: Phrase = Phrase(input)
+        //reads each String from the phrase given.
         for(i in 0..p.split.size-1){
+
+            //this array should be [1,1,1] if the String is a valid word.
             var checks = IntArray(3)
             checks[0] = checkLetters(p.split[i])
             checks[1] = checkUpper(p.split[i])
             checks[2] = isEven(p.split[i])
 
+            //Higher order function that adds up the checked array. If the sum is 3,
+            //it is a valid word.
             val total: Int = checks.reduce{sum, element -> sum + element}
             if(total == 3){
+                //If a valid word is found, and is not already in the valid HashSet of words, add it.
                 if(!words.foundWords.contains(p.split[i])) {
                     words.foundWords.add(p.split[i])
                     continue
                 }
+                //If a valid word is found and it is already in the HashSet of valid words,
+                //we know that more that one of the words occur so we check to see if it is the longest
+                //word found so far.
                 else if(words.foundWords.contains(p.split[i])){
                     if(p.split[i].length > words.longest.length)
                         words.longest = p.split[i]
@@ -114,6 +166,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        //Prints the longest word found in the phrase as an error log.
         Log.e("MainActivity", ""+words.longest)
     }
 }
